08067936e5272957ec18ad68bdc0159b33785fbf
AnaGobe <166333916+AnaGobe@users.noreply.github.com>
2024-11-25 03:34:29 +0400

    Update While.java

--- While.java
+++ While.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class While {
         public static void main(String[] args) {
-            String word = "sdshbdsbddsszhbuhhhiuh";
+            String word = "sdshbdsbddssz";
             int positionOfTheSymbol = 2;
 
             while (positionOfTheSymbol < word.length()) {
